34900f8be701ed671c8c65f064265e79839d541e
文孝礼 <873619879@qq.com>
2022-08-09 23:48:09 +0800

    feat: readOnly2

--- medium.ts
+++ medium.ts
@@ -6,3 +6,22 @@ type TestGetReturnType = GetReturnType<typeof add>
 type MyOmit<T,K extends keyof T>=Pick<T,Exclude<keyof T,K>>
 type TestMyOmit = MyOmit<{a:1,b:2,c:3},'a'|'b'>
 
+type MyReadonly2<T,K extends keyof T = keyof T>={
+    readonly [P in K]:T[P]
+} & {
+    [P in Exclude<keyof T,K>]:T[P]
+}
+
+interface Todo {
+    title: string
+    description: string
+    completed: boolean
+}
+
+const todo: MyReadOnly2<Todo, 'title' | 'description'> = {
+    title: "Hey",
+    description: "foobar",
+    completed: false,
+}
+
+type TestReadOnly2 = MyReadonly2<Todo, 'title' | 'description'>
